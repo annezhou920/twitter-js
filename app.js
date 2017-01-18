71bb1bf8658bf018923e87f954c24c36c7dc4965
Anne Zhou <anne.zhou920@gmail.com>
2017-01-18 00:35:45 -0500

    updated

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+const morgan = require('morgan'); // tool that helps create logging middleware
 const nunjucks = require('nunjucks');
-const routes = require('./routes/');
+const routes = require('./routes/'); // links to modular routing
 const app = express(); // creates an instance of an express application
-
+const bodyParser = require('body-parser');
 const socketio = require('socket.io');
+
 // ...
 var server = app.listen(3000, function(){ console.log('The server started')});
 var io = socketio.listen(server);
@@ -14,21 +15,34 @@ var io = socketio.listen(server);
 //   console.log('The server started.');
 // });
 
-app.use(function(req, res, next) { // registers for every incoming request
-  console.log('Request:', req.method, req.path, res.statusCode);
-  next();
-});
+app.use(morgan('dev')); //logging
+// registers for every incoming request (essentially this is what morgan is doing)
+// app.use(function(req, res, next) {
+//   console.log('Request:', req.method, req.path, res.statusCode);
+//   next();
+// });
+
+// middleware function to start serving images, CSS files, and JS files directly
+// instead of using res.sendFile for each file
+// everything we put in public will be automatically accessible via URI path
+app.use(express.static('public')); // just put 'public' in quotes, no backlash
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })); // for HTML for submits
+
+// parse application/json - AJAX request
+app.use(bodyParser.json());
 
 app.use('/special/', function(req, res, next) {
   res.send('you reached the special area');
   next();
 });
 
-app.use('/', routes(io));
+app.use('/', routes(io)); // router that express gives you is a middleware function
 
 app.set('view engine', 'html'); // have res.render work with html
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', { noCache: true });
+nunjucks.configure('views', { noCache: true }); // where to find the views
 
 // nunjucks.render('index.html', locals, function (err, output) {
 //     console.log(output);
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,9 @@
-
-const bodyParser = require('body-parser');
 const express = require('express');
-const router = express.Router();
-// could use one line instead: const router = require('express').Router();
+const router = express.Router(); // a function that returns new router instance (entity that can self customize a bunch of methods)
 const tweetBank = require('../tweetBank');
 
 
 module.exports = function(io){
-  // parse application/x-www-form-urlencoded
-  router.use(bodyParser.urlencoded({ extended: false }))
-
-  // parse application/json
-  router.use(bodyParser.json())
-
-  // middleware function to start serving images, CSS files, and JS files directly
-  // instead of using res.sendFile for each file
-  // everything we put in public will be automatically accessible via URI path
-  router.use(express.static('public')); // just put 'public' in quotes, no backlash
 
   router.get('/', function (req, res, next) {
     let tweets = tweetBank.list();
@@ -34,18 +21,12 @@ module.exports = function(io){
   router.get('/users/:name', function(req, res) {
     var name = req.params.name;
     var list = tweetBank.find( {name: name} );
-    // var list = tweetBank.find( function(o){
-    //   return o.name === name;
-    // });
     res.render('index', { tweets: list, showForm: true, name: name } );
   });
 
   router.get('/tweets/:id', function(req, res) {
-    var id = +req.params.id;
+    var id = +req.params.id; // coerce str id to number
     var tweet = tweetBank.find( {id: id} );
-    // var tweet = tweetBank.find( function(o){
-    //   return o.id === id;
-    // });
     res.render('index', { tweets: tweet, showForm: true } );
   });
   // router.get('/public/stylesheets', function(req, res, next){
@@ -56,3 +37,5 @@ module.exports = function(io){
 
   return router;
 }
+
+// module.exports = router;
